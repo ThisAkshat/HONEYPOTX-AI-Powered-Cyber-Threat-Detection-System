02170d0f918d636d78321f379ab5d19c2b45f79e
Akshat_sh <akshatsharma1313@gmail.com>
2026-04-21 15:03:52 +0530

    Add config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Use of config.py → Stores configuration settings like database URL, server port, model paths, and constants.
+
+MONGO_URI = "mongodb://localhost:27017"
+DB_NAME = "honeypotx"
+
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 8000
+
+MODEL_PATH = "ai_engine/model.pkl"
